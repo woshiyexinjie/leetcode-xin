f520b2151cf1abf9dc53f299ad3a91515da3a28b
yexinjie <yexinjie@souche.com>
2017-12-19 15:18:01 +0800

    ADD  Searche Integer Position algorithm leetcode

---
+++ src/main/java/com/helloxin/leetcode/algorithms/SearchInsertPosition.java
@@ -0,0 +1,44 @@
+package com.helloxin.leetcode.algorithms;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+
+ You may assume no duplicates in the array.
+
+ Example 1:
+
+ Input: [1,3,5,6], 5
+ Output: 2
+ Example 2:
+
+ Input: [1,3,5,6], 2
+ Output: 1
+ Example 3:
+
+ Input: [1,3,5,6], 7
+ Output: 4
+ Example 1:
+
+ Input: [1,3,5,6], 0
+ Output: 0
+ */
+
+/**
+ * create by nandiexin on 2017/12/19
+ **/
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+        if(  null == nums  || nums.length == 0){
+            return 0;
+        }
+        int i=0;
+        for (;i<nums.length;i++){
+           if(target == nums[i] || target < nums[i]){
+               return i;
+           }
+        }
+        return i;
+    }
+}

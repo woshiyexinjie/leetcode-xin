22ad901bd668e4e63993540403954380f3559ff0
yexinjie <yexinjie@souche.com>
2017-12-19 10:48:59 +0800

    DD  Implement_strStr algorithm question leetCode

---
+++ src/main/java/com/helloxin/leetcode/algorithms/Implement_strStr.java
@@ -0,0 +1,49 @@
+package com.helloxin.leetcode.algorithms;
+
+/**
+ * create by nandiexin on 2017/12/19
+ **/
+public class Implement_strStr {
+
+    /**
+     * 假如使用 string 的函数
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public static int strStr(String haystack, String needle) {
+           return haystack.indexOf(needle) ;
+    }
+
+    /**
+     * 用两个for循环 去解决问题  如果字符串长了  要匹配多久呢？
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public static int strStr2(String haystack, String needle) {
+
+        int m = haystack.length(), n = needle.length();
+        if (m < n) {
+            return -1;
+        }
+        for (int i = 0; i <= m - n; ++i) {
+            int j = 0;
+            for (j = 0; j < n; ++j) {
+                if (haystack.charAt(i + j) != needle.charAt(j)){
+                    break;
+                }
+            }
+            if (j == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(strStr("hello","ll"));
+    }
+}

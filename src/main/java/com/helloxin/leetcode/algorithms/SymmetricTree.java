f9995045cbc0f902d2ba578dd5badbe6243b2586
yexinjie <yexinjie@souche.com>
2018-01-04 16:44:06 +0800

    ADD Symmetric Tree algorithm leetcode

---
+++ src/main/java/com/helloxin/leetcode/algorithms/SymmetricTree.java
@@ -0,0 +1,71 @@
+package com.helloxin.leetcode.algorithms;
+
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ */
+
+import com.helloxin.leetcode.algorithms.preparation.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * create by nandiexin on 2018/1/4
+ **/
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        return (t1.val == t2.val)
+                && isMirror(t1.right, t2.left)
+                && isMirror(t1.left, t2.right);
+    }
+
+    public boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+
+}

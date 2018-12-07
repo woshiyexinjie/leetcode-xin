95475ce06236e5417ee90cdf40e9b5d307e6ea40
叶信节 <yexinjie@souche.com>
2018-12-07 13:56:49 +0800

    add ImplementQueueUsingStacks232 algorithm

---
+++ src/main/java/com/helloxin/leetcode/algorithms/ImplementQueueUsingStacks232.java
@@ -0,0 +1,56 @@
+package com.helloxin.leetcode.algorithms;
+
+import java.util.Stack;
+
+/**
+ * Created by nandiexin on 2018/12/7.
+ *
+ Implement the following operations of a queue using stacks.
+
+ push(x) -- Push element x to the back of queue.
+ pop() -- Removes the element from in front of queue.
+ peek() -- Get the front element.
+ empty() -- Return whether the queue is empty.
+ Example:
+
+ MyQueue queue = new MyQueue();
+
+ queue.push(1);
+ queue.push(2);
+ queue.peek();  // returns 1
+ queue.pop();   // returns 1
+ queue.empty(); // returns false
+ Notes:
+
+ You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+ Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+ You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+ */
+public class ImplementQueueUsingStacks232 {
+
+
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+
+    public int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}

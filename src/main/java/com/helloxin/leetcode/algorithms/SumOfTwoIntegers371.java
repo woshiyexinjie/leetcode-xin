46c8822e539ba4ad4cec90d24ba7f0b990b2cd76
叶信节 <yexinjie@souche.com>
2018-12-26 20:41:14 +0800

    add sum of two integers 371  leetcode

---
+++ src/main/java/com/helloxin/leetcode/algorithms/SumOfTwoIntegers371.java
@@ -0,0 +1,54 @@
+package com.helloxin.leetcode.algorithms;
+
+/**
+ *
+ Created by nandiexin on 2018/12/26.
+ Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+ Example 1:
+
+ Input: a = 1, b = 2
+ Output: 3
+ Example 2:
+
+ Input: a = -2, b = 3
+ Output: 1
+
+ */
+public class SumOfTwoIntegers371 {
+
+
+    //不能使用 + -
+    public static int getSum(int a, int b) {
+
+        return b==0? a:getSum(a^b, (a&b)<<1);
+    }
+
+    public static int getSum2(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        if (b == 0) {
+            return a;
+        }
+
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(getSum(2,1));
+
+    }
+
+}

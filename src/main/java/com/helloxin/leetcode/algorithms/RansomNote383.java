56ce6c62585e2812699dad184e21ec0aa9adfca6
叶信节 <yexinjie@souche.com>
2019-01-08 14:44:42 +0800

    ransom note 383 leetcode

---
+++ src/main/java/com/helloxin/leetcode/algorithms/RansomNote383.java
@@ -0,0 +1,43 @@
+package com.helloxin.leetcode.algorithms;
+
+/**
+ * Created by nandiexin on 2019/1/8.
+ *
+ Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+ Each letter in the magazine string can only be used once in your ransom note.
+
+ Note:
+ You may assume that both strings contain only lowercase letters.
+
+ canConstruct("a", "b") -> false
+ canConstruct("aa", "ab") -> false
+ canConstruct("aa", "aab") -> true
+ */
+public class RansomNote383 {
+
+    //原来这道题是这个意思 用map处理也是一样的
+    public static boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] arr = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(--arr[ransomNote.charAt(i)-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(canConstruct("a", "b"));
+        System.out.println(canConstruct("aa", "ab"));
+        System.out.println(canConstruct("aa", "aab"));
+
+
+    }
+}

f88d5e2e2d00cfe68132fbb4313917f7a399c6a6
叶信节 <yexinjie@souche.com>
2019-02-22 17:26:51 +0800

    ADD  keyboardRow leetcode 5006

---
+++ src/main/java/com/helloxin/leetcode/algorithms/KeyboardRow500.java
@@ -0,0 +1,40 @@
+package com.helloxin.leetcode.algorithms;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+/**
+ * Created by nandiexin on 2019/2/22.
+ */
+public class KeyboardRow500 {
+
+    public String[] findWords(String[] words) {
+        return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+    }
+
+    public String[] findWords2(String[] words) {
+        String[] strs = {"QWERTYUIOP","ASDFGHJKL","ZXCVBNM"};
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i<strs.length; i++){
+            for(char c: strs[i].toCharArray()){
+                map.put(c, i);//put <char, rowIndex> pair into the map
+            }
+        }
+        List<String> res = new LinkedList<>();
+        for(String w: words){
+            if(w.equals("")) continue;
+            int index = map.get(w.toUpperCase().charAt(0));
+            for(char c: w.toUpperCase().toCharArray()){
+                if(map.get(c)!=index){
+                    index = -1; //don't need a boolean flag.
+                    break;
+                }
+            }
+            if(index!=-1) res.add(w);//if index != -1, this is a valid string
+        }
+        return res.toArray(new String[0]);
+    }
+}

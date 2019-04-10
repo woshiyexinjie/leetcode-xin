551fe6909e507246c6e053011174fbfa605748b9
叶信节 <yexinjie@souche.com>
2019-04-10 22:42:40 +0800

    FIX leetcode  MinCostClimbingStairs746

---
+++ src/main/java/com/helloxin/leetcode/algorithms/MinCostClimbingStairs746.java
@@ -0,0 +1,41 @@
+package com.helloxin.leetcode.algorithms;
+
+/**
+ * Created by nandiexin on 2019/4/10.
+ * <p>
+ * <p>
+ * 数组的每个索引做为一个阶梯，第 i个阶梯对应着一个非负数的体力花费值 cost[i](索引从0开始)。
+ * <p>
+ * 每当你爬上一个阶梯你都要花费对应的体力花费值，然后你可以选择继续爬一个阶梯或者爬两个阶梯。
+ * <p>
+ * 您需要找到达到楼层顶部的最低花费。在开始时，你可以选择从索引为 0 或 1 的元素作为初始阶梯。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: cost = [10, 15, 20]
+ * 输出: 15
+ * 解释: 最低花费是从cost[1]开始，然后走两步即可到阶梯顶，一共花费15。
+ * 示例 2:
+ * <p>
+ * 输入: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * 输出: 6
+ * 解释: 最低花费方式是从cost[0]开始，逐个经过那些1，跳过cost[3]，一共花费6。
+ * 注意：
+ * <p>
+ * cost 的长度将会在 [2, 1000]。
+ * 每一个 cost[i] 将会是一个Integer类型，范围为 [0, 999]
+ */
+public class MinCostClimbingStairs746 {
+
+    /**
+     *看了别人的解答才知道 原来这道题目是这个意思
+     * 但是也好难理解呀  选择继续爬一个阶梯或者爬两个阶梯  这个费力不费力是怎么计算的呀  真是难以理解呀
+     * 要是理解成买个台阶有多少钉子 踩中的最少的概率的方式 这样好像还可以
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        for (int i = 2; i < cost.length; i++) {
+            cost[i] += Math.min(cost[i-1], cost[i-2]);
+        }
+        return Math.min(cost[cost.length-1], cost[cost.length-2]);
+    }
+}

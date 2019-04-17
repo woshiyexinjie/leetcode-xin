13e0b6d3583f3258d0232970e91a3ad1969d979e
叶信节 <yexinjie@souche.com>
2019-04-17 21:17:46 +0800

    FIX leetcode  reach a numbser 754

---
+++ src/main/java/com/helloxin/leetcode/algorithms/ReachANumber754.java
@@ -0,0 +1,75 @@
+package com.helloxin.leetcode.algorithms;
+
+/**
+ * Created by nandiexin on 2019/4/17.
+ * <p>
+ * <p>
+ * <p>
+ * You are standing at position 0 on an infinite number line. There is a goal at position target.
+ * <p>
+ * On each move, you can either go left or right. During the n-th move (starting from 1), you take n steps.
+ * <p>
+ * Return the minimum number of steps required to reach the destination.
+ * <p>
+ * Example 1:
+ * Input: target = 3
+ * Output: 2
+ * Explanation:
+ * On the first move we step from 0 to 1.
+ * On the second step we step from 1 to 3.
+ * Example 2:
+ * Input: target = 2
+ * Output: 3
+ * Explanation:
+ * On the first move we step from 0 to 1.
+ * On the second move we step  from 1 to -1.
+ * On the third move we step from -1 to 2.
+ * Note:
+ * target will be a non-zero integer in the range [-10^9, 10^9].
+ * 在一根无限长的数轴上，你站在0的位置。终点在target的位置。
+ * <p>
+ * 每次你可以选择向左或向右移动。第 n 次移动（从 1 开始），可以走 n 步。
+ * <p>
+ * 返回到达终点需要的最小移动次数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: target = 3
+ * 输出: 2
+ * 解释:
+ * 第一次移动，从 0 到 1 。
+ * 第二次移动，从 1 到 3 。
+ * 示例 2:
+ * <p>
+ * 输入: target = 2
+ * 输出: 3
+ * 解释:
+ * 第一次移动，从 0 到 1 。
+ * 第二次移动，从 1 到 -1 。
+ * 第三次移动，从 -1 到 2 。
+ */
+public class ReachANumber754 {
+
+
+    public static int reachNumber(int target) {
+
+        target = Math.abs(target);
+        int step = 0;
+        int sum = 0;
+        while (sum < target) {
+            step++;
+            sum += step;
+        }
+        while ((sum - target) % 2 != 0) {
+            step++;
+            sum += step;
+        }
+        return step;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reachNumber(4));
+    }
+
+
+}

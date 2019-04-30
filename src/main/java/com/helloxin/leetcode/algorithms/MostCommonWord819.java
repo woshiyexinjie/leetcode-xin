b0ecb10331136d994c91ae00496d34080ac872a1
叶信节 <yexinjie@souche.com>
2019-04-30 10:43:10 +0800

    ADD leetcode  MostCommonWord819

---
+++ src/main/java/com/helloxin/leetcode/algorithms/MostCommonWord819.java
@@ -0,0 +1,61 @@
+package com.helloxin.leetcode.algorithms;
+
+import java.util.*;
+
+/**
+ * Created by nandiexin on 2019/4/29.
+ * 给定一个段落 (paragraph) 和一个禁用单词列表 (banned)。返回出现次数最多，同时不在禁用列表中的单词。题目保证至少有一个词不在禁用列表中，而且答案唯一。
+ * <p>
+ * 禁用列表中的单词用小写字母表示，不含标点符号。段落中的单词不区分大小写。答案都是小写字母。
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入:
+ * paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+ * banned = ["hit"]
+ * 输出: "ball"
+ * 解释:
+ * "hit" 出现了3次，但它是一个禁用的单词。
+ * "ball" 出现了2次 (同时没有其他单词出现2次)，所以它是段落里出现次数最多的，且不在禁用列表中的单词。
+ * 注意，所有这些单词在段落里不区分大小写，标点符号需要忽略（即使是紧挨着单词也忽略， 比如 "ball,"），
+ * "hit"不是最终的答案，虽然它出现次数更多，但它在禁用单词列表中。
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 1 <= 段落长度 <= 1000.
+ * 1 <= 禁用单词个数 <= 100.
+ * 1 <= 禁用单词长度 <= 10.
+ * 答案是唯一的, 且都是小写字母 (即使在 paragraph 里是大写的，即使是一些特定的名词，答案都是小写的。)
+ * paragraph 只包含字母、空格和下列标点符号!?',;.
+ * 不存在没有连字符或者带有连字符的单词。
+ * 单词里只包含字母，不会出现省略号或者其他标点符号。
+ */
+public class MostCommonWord819 {
+
+    public String mostCommonWord(String p, String[] banned) {
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> count = new HashMap<>();
+        String[] words = p.replaceAll("\\W+" , " ").toLowerCase().split("\\s+");
+        for (String w : words) {
+            if (!ban.contains(w)) {
+                count.put(w, count.getOrDefault(w, 0) + 1);
+            }
+        }
+        return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+
+    public String mostCommonWord2(String paragraph, String[] banned) {
+        String[] words = paragraph.toLowerCase().split("[ !?',;.]+");
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
+        for(String word : banned) if(map.containsKey(word)) map.remove(word);
+        String res = null;
+        for(String word : map.keySet())
+            if(res == null || map.get(word) > map.get(res))
+                res = word;
+        return res;
+    }
+}

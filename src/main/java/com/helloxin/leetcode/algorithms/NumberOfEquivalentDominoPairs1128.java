03a2220bee8dd418b0319850bbc8f246fdb15565
叶信节 <yexinjie@souche.com>
2019-12-17 13:00:04 +0800

    ADD 1128 1122

---
+++ src/main/java/com/helloxin/leetcode/algorithms/NumberOfEquivalentDominoPairs1128.java
@@ -0,0 +1,33 @@
+package com.helloxin.leetcode.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yebanxian on 2019/12/17.
+ * Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a==c and b==d), or (a==d and b==c) - that is, one domino can be rotated to be equal to another domino.
+ * <p>
+ * Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
+ * Output: 1
+ */
+public class NumberOfEquivalentDominoPairs1128 {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int cnt = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] a : dominoes) {
+            int max = Math.max(a[0], a[1]), min = Math.min(a[0], a[1]);
+            int key = min * 10 + max;
+            int pairs = map.getOrDefault(key, 0); // the number of dominoes already in the map is the number of the newly found pairs.
+            cnt += pairs;
+            map.put(key, 1 + pairs);
+
+        }
+        return cnt;
+    }
+}
--- src/main/java/com/helloxin/leetcode/algorithms/RelativeSortArray1122.java
+++ src/main/java/com/helloxin/leetcode/algorithms/RelativeSortArray1122.java
@@ -24,21 +24,20 @@ package com.helloxin.leetcode.algorithms;
 public class RelativeSortArray1122 {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         int[] cnt = new int[1001];
-        for(int n : arr1) {
+        for (int n : arr1) {
             cnt[n]++;
         }
         int i = 0;
-        for(int n : arr2) {
-            while(cnt[n]-- > 0) {
+        for (int n : arr2) {
+            while (cnt[n]-- > 0) {
                 arr1[i++] = n;
             }
         }
-        for(int n = 0; n < cnt.length; n++) {
-            while(cnt[n]-- > 0) {
+        for (int n = 0; n < cnt.length; n++) {
+            while (cnt[n]-- > 0) {
                 arr1[i++] = n;
             }
         }
         return arr1;
     }
-
 }
